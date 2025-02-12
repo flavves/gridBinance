018e40206cfbfbe4251ce21e542fd22c55711a27
Batuhan ÖKMEN <45328109+flavves@users.noreply.github.com>
2025-02-12 17:12:19 +0300

    Binance Access Limit for Runner
    That's why Runner operation is being tried with Mock.

--- tests/test_binance_trader.py
+++ tests/test_binance_trader.py
@@ -1,11 +1,12 @@
 import sys
 import os
+import pytest
+from unittest.mock import MagicMock, patch
+
+# Proje dizinini sys.path'e ekleyerek modüllerin düzgün yüklenmesini sağlıyoruz.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
-import pytest
 from BinanceTrader import BinanceTrader
-from unittest.mock import MagicMock
 
 @pytest.fixture
 def trader():
@@ -14,11 +15,19 @@ def trader():
     db_file_path = "test_trades.json"
     telegram_bot_token = "test_telegram_bot_token"
     telegram_chat_id = "test_telegram_chat_id"
-    trader = BinanceTrader(api_key, api_secret, db_file_path, telegram_bot_token, telegram_chat_id)
-    trader.client.create_order = MagicMock(return_value={"status": "success"})
-    trader.db_manager.add_trade = MagicMock(return_value="Trade added successfully")
-    trader.telegram_sender.send_message = MagicMock(return_value="Message sent successfully")
-    return trader
+
+    with patch("BinanceTrader.Client") as mock_client:
+        mock_client_instance = mock_client.return_value
+        mock_client_instance.create_order = MagicMock(return_value={"status": "success"})
+        
+        trader = BinanceTrader(api_key, api_secret, db_file_path, telegram_bot_token, telegram_chat_id)
+        trader.client = mock_client_instance
+        
+        # Diğer bağımlılıkları da mock'luyoruz
+        trader.db_manager.add_trade = MagicMock(return_value="Trade added successfully")
+        trader.telegram_sender.send_message = MagicMock(return_value="Message sent successfully")
+        
+        return trader
 
 def test_buy_order(trader):
     response = trader.buy("BTCUSDT", "LIMIT", 0.001, 50000)
